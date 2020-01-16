30ead071ba239c724dcb519e759fc6d004ba10cd
Edwin G <edwgeo@gmail.com>
2020-01-15 23:45:37 -0800

    Getting sentiment of tweet

--- senAnaly.py
+++ senAnaly.py
@@ -23,3 +23,12 @@ class TwitterClient(object):
 	def clean_tweet(self, tweet):
 		return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
 
+	def get_sentiment(self, tweet):  
+		analysis = TextBlob(self.clean_tweet(tweet)) 
+		if analysis.sentiment.polarity > 0: 
+			return 'positive'
+		elif analysis.sentiment.polarity == 0: 
+			return 'neutral'
+		else: 
+			return 'negative'
+
